4bb888e8078d644d74de46db194640842b81944f
Judy <judy.zhu@learnosity.com>
2020-09-11 15:03:04 +1000

    Add createTodo

--- controllers/todo.ts
+++ controllers/todo.ts
@@ -1,4 +1,5 @@
 import todos from "../stubs/todos.ts";
+import { v4 } from "https://deno.land/std/uuid/mod.ts";
 
 export default {
     getAllTodos: ({ response }: { response: any }) => {
@@ -8,7 +9,27 @@ export default {
             data: todos
         }
     },
-    createTodo: async() => {},
+    createTodo: async({ request, response } : { request: any; response: any }) => {
+        const body = await request.body();
+        if (!request.hasBody) {
+            response.status = 400;
+            response.body = {
+                success: false,
+                message: "No data provided",
+            };
+            return;
+        }
+
+        let newTodo = {
+            id: v4.generate(),
+            todo: body.value.todo,
+            isCompleted: false
+        };
+        response.body = {
+            success: true,
+            data: [...todos, newTodo],
+        }
+    },
     getTodoById: () => {},
     updateTodoById: () => {},
     deleteTodoById: () => {},
